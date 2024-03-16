66dce10f9b1a5f4c50b117705d99fce161ab1a32
Andreas <andreas221b@gmail.com>
2024-03-16 10:46:18 +0100

    disable platform specific

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,19 @@
 
 //local headers
 #include "application/application.h"
-#include "platform/win32_main.h"
 
+/*
 #ifdef __linux__
 #include "platform/linux_main.h"
 #else
 #include "platform/win32_main.h"
 #endif
+*/
 
 using namespace std;
 
 int main() {
-#ifdef _WIN32
-  return WinMain(GetModuleHandleA(nullptr), nullptr, GetCommandLineA(), SW_SHOWDEFAULT);
-#else
+
   return 0;
-#endif
 
 }
