29e02487791502e30c7bb200ec013577f6c3a3d5
Andreas <andreas221b@gmail.com>
2024-03-16 11:35:22 +0100

    update project

---
+++ src/main/animaX.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "application/application.h"
+
+/*
+#include <KHR/khrplatform.h>
+#ifdef __linux__
+#include "platform/linux_main.h"
+#else
+#include "platform/win32_main.h"
+#endif
+*/
+
+using namespace std;
+
+int main() { return 0; }
